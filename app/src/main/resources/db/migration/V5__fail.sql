2accdd1867a86500d5d21ebe38fab6698b822047
Ahmad Morad <ahmadmoradhr@gmail.com>
2025-06-01 22:52:23 +0200

    back to my commit

---
+++ app/src/main/resources/db/migration/V5__fail.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test (
+                                    id SERIAL PRIMARY KEY,
+                                    name VARCHAR(100) NOT NULL,
+    salary VARCHAR(100) NOT NULL
+    );
