2268ea4aabfef964279b253a07440f8d68fa7ee7
Morad, Ahmad <ahmad.morad@adesso.de>
2025-06-12 13:40:41 +0200

    liquibase worked

--- app/src/main/java/online/ahmadmorad/flyaway_vs_liquibase/FlyawayVsLiquibaseApplication.java
+++ app/src/main/java/online/ahmadmorad/flyaway_vs_liquibase/FlyawayVsLiquibaseApplication.java
@@ -1,13 +1,23 @@
 package online.ahmadmorad.flyaway_vs_liquibase;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlyawayVsLiquibaseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FlyawayVsLiquibaseApplication.class, args);
 	}
 
+	@PostConstruct
+	public void checkLiquibaseFiles() {
+		try {
+			System.out.println("Checking files...");
+			System.out.println("MASTER: " + getClass().getResource("/db/changelog/master.xml"));
+			System.out.println("V1: " + getClass().getResource("/db/changelog/V1__init.xml"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
